2e150c84c2999100f74f8d9acf3c48901ac2e3bc
Williams Medina <williams.medinaa@gmail.com>
2016-04-27 11:48:38 -0400

    adding node installer

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # formulas
 
 Useful scripts to setup and install development tools
+
+
+## NodeJS
+
+### Install Node
+
+```
+curl -o- url.sh | bash
+```
---
+++ node/install.sh
@@ -0,0 +1,50 @@
+# usage: ./node.sh 5.11.0
+OS=`uname`
+DISTRO=""
+VERSION=$1
+
+detect_version ()
+{
+  if [ $# -ne "1" ]
+  then
+    echo "Usage: `basename $0` <version>"
+    exit 1
+  fi
+}
+
+
+detect_os ()
+{
+  if [[ "$OS" =~ "Darwin" ]]; then
+    DISTRO=node-v${VERSION}-darwin-x64
+  else
+    DISTRO=node-v${VERSION}-linux-x64
+  fi
+}
+
+main ()
+{
+  detect_os
+  detect_version
+
+  rm -rf /usr/local/bin/node /usr/bin/node /usr/local/bin/npm /usr/bin/npm /tmp/node-*
+
+  FILE=${DISTRO}.tar.gz
+
+  cd /tmp
+
+  curl -O http://nodejs.org/dist/v${VERSION}/$FILE
+  tar -zxfv $FILE
+  mkdir -p /usr/local/bin/
+
+  ln -s /tmp/$DISTRO/bin/node /usr/local/bin/node
+  ln -s /tmp/$DISTRO/bin/npm /usr/local/bin/npm
+
+  rm -rf /usr/local/lib/node_modules
+  rm $FILE
+
+  npm config set prefix /usr/local
+  node --version
+}
+
+main
